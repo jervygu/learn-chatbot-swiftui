907fef5ee16697f7982f35c5d8cf92c6e8fa82b7
Jervy Umandap <jegumandap@unionbankph.com>
2024-08-19 12:08:23 +0800

    ### Starter project

---
+++ ChatBot/ChatBot/ChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotApp.swift
+//  ChatBot
+//
+//  Created by Jervy Umandap on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
